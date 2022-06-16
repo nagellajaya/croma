6c65def6cc1774b99722ca471924954e7ecd1a7e
root <root@ip-172-31-29-96.us-east-2.compute.internal>
2022-06-16 17:26:27 +0000

    jaya is developed

---
+++ sasi.java
@@ -0,0 +1,11 @@
+noru muskoni padukundam ika
+chesimdi chalu kani
+nannu thittadanki baga noru vastahdi niku
+nv cheppindi chesina kopama
+spacelu evvaku annav anega
+ala kottindi
+sarelebba nike pedda vachhu kani
+devops lo mithun cousin brother vi le
+nenu kadanaledu
+sarena
+naku artamayyela opikaga cheppuyou clearly understood what iam saying
